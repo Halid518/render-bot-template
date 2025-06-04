df6bb13bf6714d5c00900d81bead4f415c99df98
Halid518 <vadimlevadniy00@mail.ru>
2025-06-05 04:29:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,28 @@
 import telebot
 from flask import Flask, request
 
-API_TOKEN = import telebot
-from flask import Flask, request
-
-API_TOKEN = import telebot
-from flask import Flask, request
-
-API_TOKEN = @ShopBonus1_bot
+API_TOKEN = '7928610467:AAFjTmq_R0OIhRrRDXVnuChgKQKP5-UOSDI'
 
 bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
+
+# Главная страница для теста
+@app.route('/')
+def index():
+    return "Бот работает!", 200
 
 # Ответ на любое сообщение
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-# Обработка webhook
+# Обработка Webhook от Telegram
 @app.route(f'/{API_TOKEN}', methods=['POST'])
 def webhook():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
+    update = telebot.types.Update.de_json(request.get_data().decode('utf-8'))
     bot.process_new_updates([update])
-    return "OK", 200
+    return '', 200
 
-# Тестовая главная страница
-@app.route('/')
-def index():
-    return "Бот работает!", 200
-
-if __name__ == '__main__':
-    app.run()
-
-bot = telebot.TeleBot(API_TOKEN)
-app = Flask(__name__)
-
-# Ответ на любое сообщение
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-# Обработка webhook
-@app.route(f'/{API_TOKEN}', methods=['POST'])
-def webhook():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "OK", 200
-
-# Тестовая главная страница
-@app.route('/')
-def index():
-    return "Бот работает!", 200
-
-if __name__ == '__main__':
-    app.run()
-
-bot = telebot.TeleBot(API_TOKEN)
-app = Flask(__name__)
-
-# Ответ на любое сообщение
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-# Обработка webhook
-@app.route(f'/{API_TOKEN}', methods=['POST'])
-def webhook():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "OK", 200
-
-# Тестовая главная страница
-@app.route('/')
-def index():
-    return "Бот работает!", 200
-
+# Локальный запуск (если нужно)
 if __name__ == '__main__':
     app.run()
